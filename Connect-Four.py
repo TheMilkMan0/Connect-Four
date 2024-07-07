3bbcd2556c848c92e94f7379822df482ae1f3d77
Alex Myers <alexcmyers@arizona.edu>
2024-07-06 17:30:13 -0700

    I added and implemented a forward check for win system (function) that has a customizeable option for changing the number of peice to check for in a row, Ive tried to make the function as effeiciant as possible by cutting off it off and returning a lose when possible

--- Connect-Four.py
+++ Connect-Four.py
@@ -1,3 +1,6 @@
+import os 
+import sys 
+
 # ANSI escape codes for colors
 class colors:
     RED = '\033[91m'
@@ -33,7 +36,7 @@ def display_board(board):
     # NOTE: There can only be values in the board that are values in the color key or else error 
 
     # Print the matching numbers to columns on top 
-    for k in range(len(board[0])):
+    for k in range(1,len(board[0])+1):
         print(" " + str(k) + " ",end='')
     print()
 
@@ -50,25 +53,110 @@ def display_board(board):
             print('[' + display_item + ']',end='')
         print()
 
-# def pre_game():
-#     board_width = 7
-#     board_height = 6
-#     print(f"Demensions of board: {board_width},{board_height}")
-#     player1_color = colors.YELLOW
-#     player1_peice = (player1_color + "●" + colors.END)
-#     player2_color = colors.RED
-#     player2_peice = (player2_color + "●" + colors.END)
-#     print(f'Player 1 Color: Yellow {player1_peice} \nPlayer 2 Color: Red {player2_peice}')
-#     print('Starting game now...\n\n')
+# CHAT GPT CODE:
+def get_key():
+    if os.name == 'nt':
+        import msvcrt
+        return msvcrt.getch().decode('utf-8')
+    else:
+        import tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            key = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return key
 
-#     game(board_width,board_height,player1_color,player2_color)
+
+def get_valid_move(board):
+    '''
+    This function will ask the user to press a key to represent 
+    the column they wish to have their peice placed. 
+    The visual columns presented to the user start with the number 1. 
+    while the return value of this function will be ajusted for the column chosen to be in index form. 
+
+    :param board: the master board (2d List) with integers stored as players peices locations
+    :type board: list (2d list)
+
+    :rtype: int
+    :return: integer representing column converted in the index form of the master board 
+    '''
+    # Constatnly Run until the user has a proper selection 
+    while True:
+        # use try here because the program will try to convert the user input to a integer 
+        try:
+            print(("Enter column (1-7): "))
+            # Use the get_key function to instantly get the integer of the key pressed without the user having to press enter 
+            column = int(get_key()) - 1
+            # if the integer pressed is inside the culumn bounds of the board and the top most place of the column does not have a peice in it then continue 
+            if 0 <= column < len(board[0]) and board[0][column] == 0:
+                return column
+            # if the integer is to large or small 
+            else:
+                print("Invalid move. Try again.")
+        # if the key press was not a integer at all 
+        except:
+            print("Please enter a valid integer.")
+
+def forward_check(i,j,peice_num,board):
+    '''
+    This function will check for [required_in_a_row] in a row. 
+    Customize: [required_in_a_row] can be changed to any number, 1 in a row will presumably end instantly
+    
+    :param i: the row (sublist) we are in on the board
+    :type i: int
+    :param j: the column (index) we are in on the row of the board
+    :type j: int
+    :param peice_num: the integeger representing the player on the master board 
+    :type peice_num: int
+    :param board: the master board (2d list) with integers as players
+    :type board: list
+
+    :rtype: boolean
+    :return: True for win, False for no Win
+
+    '''
+    required_in_a_row = 3
+    # ----------------------
+    # Define a counter for tracking how many are cuently in a row from the starting point of i,j
+    peice_in_row_count = 1
+    # Count how many times we have moved forward 
+    move_forward_count = 1
+    # cut down on the number of checks by returning a auto false if the checking postion is at the end of the row, its not possible to have 4 in a row if the starting position is the 3rd from last position of the row 
+    if peice_num == 0:
+        return False
+    if j > len(board[i])-required_in_a_row:
+        return False
+    
+    # from the starting point check ahead [required_in_a_row] times
+    for _ in range(required_in_a_row-1):
+
+        # check that the next checking possiion is within bounds
+        if j + move_forward_count < len(board[i]):
+
+            # if the peice to the right (however many right) is the same as the to be checked peice then add to [peice_in_a_row] counter
+            if board[i][j+move_forward_count] == peice_num:
+                peice_in_row_count += 1
+
+                # if there were [required_in_a_row] in a row then call a win 
+                if peice_in_row_count >= required_in_a_row:
+
+                    # a return of true means there where 4 peices in a row 
+                    return True
+            # Cut this function short by return false (lose) after there wasnt another in a row, because if we are only checking [required_in_a_row] times then all of those times have to be true for it to be a win                 
+            else:
+                return False
+            move_forward_count += 1
 
 def game():
     board = create_board(7,6)
     game_over = False
     while not game_over:
         display_board(board)
-        #get_valid_move()
+        # This variable is in the list index form (min being zero)
+        column = get_valid_move(board) 
         #Update the master board()
         #Check for win 
         # which players 
--- Testing.py
+++ Testing.py
@@ -6,3 +6,74 @@ def testing_creating_board_custom_demnsions():
         board.append(row)
 
     print(board)
+
+def testing_check_for_win():
+    def create_board(width, height):
+        board = [[0,1,1,1,1,1,0],
+                 [0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0]]
+        
+        return board
+    board = create_board(7,6)
+
+    def forward_check(i,j,peice_num):
+        '''
+        This function will check for [required_in_a_row] in a row. 
+        Customize: [required_in_a_row] can be changed to any number, 1 in a row will presumably end instantly
+        
+        :param i: the row (sublist) we are in on the board
+        :type i: int
+        :param j: the column (index) we are in on the row of the board
+        :type j: int
+        :param peice_num: the integeger representing the player on the master board 
+        :type peice_num: int
+
+        :rtype: boolean
+        :return: True for win, False for no Win
+
+        '''
+        required_in_a_row = 3
+        # ----------------------
+        # Define a counter for tracking how many are cuently in a row from the starting point of i,j
+        peice_in_row_count = 1
+        # Count how many times we have moved forward 
+        move_forward_count = 1
+        # cut down on the number of checks by returning a auto false if the checking postion is at the end of the row, its not possible to have 4 in a row if the starting position is the 3rd from last position of the row 
+        if j > len(board[i])-required_in_a_row:
+            return False
+        print(f"Current Peice: {peice_num}")
+        # from the starting point check ahead [required_in_a_row] times
+        for _ in range(required_in_a_row-1):
+            print(f'{_+1}st Run')
+            # check that the next checking possiion is within bounds
+            if j + move_forward_count < len(board[i]):
+                print(f'{j}+{move_forward_count} < {len(board[i])}, WITHIN BOUNDS')
+                # if the peice to the right (however many right) is the same as the to be checked peice then add to [peice_in_a_row] counter
+                if board[i][j+move_forward_count] == peice_num:
+                    print(f'{board[i][j+move_forward_count]} == {peice_num}')
+                    peice_in_row_count += 1
+                    print(f"peice in row increased to: {peice_in_row_count}")
+                    # if there were [required_in_a_row] in a row then call a win 
+                    if peice_in_row_count >= required_in_a_row:
+                        print(f'Returning Win: there were {peice_in_row_count} in a row')
+                        # a return of true means there where 4 peices in a row 
+                        return True
+                # Cut this function short by return false (lose) after there wasnt another in a row, because if we are only checking [required_in_a_row] times then all of those times have to be true for it to be a win                 
+                else:
+                    return False
+                move_forward_count += 1
+            
+
+    # go over each item and check if the left to right is a win 
+    # for i in range(len(board)):
+    for j in range(len(board[0])):
+        # The peice that is in this spots checkings
+        peice_num = board[0][j]
+        win = forward_check(0,j,peice_num)
+        print(win)
+        print('----')
+    
+testing_check_for_win()
